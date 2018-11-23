dd85f7f7823cb6852009aecaa2850b82f2d7187a
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 00:28:57 +0200

    Door base

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	
+	private bool _isOpen = false;
+
+    public bool IsOpen
+    {
+        get
+        {
+            return _isOpen;
+        }
+
+        set
+        {
+            _isOpen = value;
+			Debug.Log(IsOpen);
+        }
+    }
+
+	void toggle() {
+		IsOpen = !IsOpen;
+	}
+
+}
