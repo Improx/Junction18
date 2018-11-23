4cc84113e129b89f9fffe6071f69f9d8b015f5cb
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 00:21:43 +0200

    Button code

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private bool _isTriggered = false;
+
+    public bool IsTriggered
+    {
+        get
+        {
+            return _isTriggered;
+        }
+
+        set
+        {
+            _isTriggered = value;
+            Debug.Log(IsTriggered);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) => IsTriggered = true;
+
+    private void OnTriggerExit(Collider other) => IsTriggered = false;
+
+}
