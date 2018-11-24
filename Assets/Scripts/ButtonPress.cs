1d5294071fd2440f8d1a5fbe606017adaa9550e0
Miika Kanerva <miika.kanerva@aalto.fi>
2018-11-24 21:09:37 +0200

    button pressed sprite & initial script that just changes the sprite

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+public class ButtonPress : MonoBehaviour {
+
+  public Sprite notPressedSprite;
+  public Sprite pressedSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    GetComponent<SpriteRenderer>().sprite = pressedSprite;
+  }
+
+  private void OnCollisionExit2D(Collision2D other) {
+    GetComponent<SpriteRenderer>().sprite = notPressedSprite;
+  }
+}
