5dbc23253fc48549f89ddb51a72501477859e26a
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 19:18:57 +0200

    Extraction gives points

---
+++ Assets/Scripts/ExtractionPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtractionPoint : MonoBehaviour {
+
+	public int points;
+
+	void OnTriggerStay2D(Collider2D other)
+    {
+        // pickup with left click if object has item tag 
+        if (other.CompareTag("Item")  && Input.GetButtonDown("Fire2"))
+        {
+			other.enabled = !other.enabled;
+			points += 1;
+        }
+    }
+
+    private void Update() => Debug.Log(points);
+
+}
