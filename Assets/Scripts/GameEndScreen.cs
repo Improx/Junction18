6fcd0a91ec2ee578cb52f5eafc0db4e6e0554a56
Jussi Joki <judejoki@gmail.com>
2018-11-25 00:25:31 +0200

    Gameendscreen script

--- Assets/Scripts/GameEndScreen.cs
+++ Assets/Scripts/GameEndScreen.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameEndScreen : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject EndScreen;
+	public TextMeshProUGUI	DetainedText;
+	public TextMeshProUGUI	StolenText;
+
+	public static GameEndScreen Instance { get; private set; }
+
+	private void Awake() {
+		Instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void Display()
+    {
+        EndScreen.SetActive(true);
+		DetainedText.text = $"Robbers detained: {GameManager.Instance.NumOfDetainedRobbers} / {GameManager.Instance.NumOfRobbers}";
+		StolenText.text = $"Items stolen: {GameManager.Instance.NumOfStolenItems} / {GameManager.Instance.NumOfItems}";
+
+    }
 }
