7bf2b875576fb3d70ddd47b61462e9d853098889
Oskiii <oliukku@gmail.com>
2018-11-24 20:59:43 +0200

    Remove robbers from All when destroyed

--- Assets/Scripts/Robber.cs
+++ Assets/Scripts/Robber.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Robber : MonoBehaviour {
+public class Robber : MonoBehaviour
+{
 
 	public float FlashlightRadiance = 0;
 
 	public static List<Robber> All = new List<Robber>();
 
 	// Use this for initialization
-	void Awake () {
+	void Awake()
+	{
 		All.Add(this);
 	}
 
+	private void OnDestroy()
+	{
+		All.Remove(this);
+	}
 }
