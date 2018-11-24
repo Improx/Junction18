656d3a56d4ea431d0fd31a5344f5cb2b8ae9b07c
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 12:45:05 +0200

    Door -> 2d

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,16 +2,16 @@
 
 public class Door : MonoBehaviour {
 	
-	Collider d_Collider;
-	MeshRenderer d_Mesh;
+	Collider2D d_Collider;
+	SpriteRenderer d_Sprite;
 	private void Start() {
-		d_Collider = GetComponent<Collider>();
-		d_Mesh = GetComponent<MeshRenderer>();
+		d_Collider = GetComponent<Collider2D>();
+		d_Sprite = GetComponent<SpriteRenderer>();
 	}
 
 	public void Toggle() {
 		d_Collider.enabled = !d_Collider.enabled;
-		d_Mesh.enabled = !d_Mesh.enabled;	
+		d_Sprite.enabled = !d_Sprite.enabled;	
 		//TODO: Add sound effect (& animation?)
 	}
 
