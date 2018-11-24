9d793abe8b1b8d3a4f7653b50c09e6afef2eab1f
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 18:49:50 +0200

    Detection indicator

--- Assets/Scripts/DetectionHandler.cs
+++ Assets/Scripts/DetectionHandler.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class DetectionHandler : MonoBehaviour {
 
 	public List<Robber> Robs;
-	public AudioSource source;
+	public AudioSource Source;
 	public AudioClip Alarm;
+	public GameObject Exclamation;
+	public SpriteRenderer ExPoint;
 	void OnEnable () => FieldOfView.Detected += PlayEffects;
 	void OnDisable () => FieldOfView.Detected -= PlayEffects;
 	void PlayEffects (Robber r, string status) {
-		if (status == "Add" && !Robs.Contains(r)) {
-			source.PlayOneShot(Alarm);
-			Robs.Add(r);
+		if (status == "Add" && !Robs.Contains (r)) {
+			Exclamation = r.transform.Find ("Exclamation").gameObject;
+			ExPoint = Exclamation.GetComponent<SpriteRenderer> ();
+			Source.PlayOneShot (Alarm);
+			StartCoroutine (FlashPoint ());
+			Robs.Add (r);
+		} else if (status == "Remove" && Robs.Contains (r)) {
+			Robs.Remove (r);
 		}
-		else if (status == "Remove" && Robs.Contains(r)) {
-			Robs.Remove(r);
-		}
+	}
+
+	public IEnumerator FlashPoint () {
+		ExPoint.enabled = !ExPoint.enabled;
+		yield return new WaitForSeconds (0.6f);
+		ExPoint.enabled = !ExPoint.enabled;
 	}
 }
