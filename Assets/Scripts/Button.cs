f6ac130cb73dbcd7fce0e0f106f9c0f36d2cb21e
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 12:42:28 +0200

    Button script to 2d

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,8 +4,8 @@ public class Button : MonoBehaviour {
 
     [SerializeField]
     public GameObject Target;
-    public void OnTriggerEnter(Collider other) => Target.GetComponent<Door>().Toggle();
+    public void OnTriggerEnter2D(Collider2D other) => Target.GetComponent<Door>().Toggle();
 
-    public void OnTriggerExit(Collider other) => Target.GetComponent<Door>().Toggle();
+    public void OnTriggerExit2D(Collider2D other) => Target.GetComponent<Door>().Toggle();
 
 }
