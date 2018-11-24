17c165e3331c1ad68d618e82ec6b60a58116e89f
Jussi Joki <judejoki@gmail.com>
2018-11-24 14:54:46 +0200

    Robber camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,24 @@
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Player : MonoBehaviour {
+public class Player : NetworkBehaviour {
     public PlayerType Team;
+
+    public CinemachineVirtualCamera RobberCameraPrefab;
+
+    private void Start() {
+        if (!isLocalPlayer) return;
+
+        if (Team == PlayerType.Robber) {
+            var mainCamera = FindObjectOfType<Camera>();
+
+            var vmCam = Instantiate(RobberCameraPrefab, new Vector3(0, 0, -100), new Quaternion());
+            vmCam.Follow = transform;
+            vmCam.m_Lens.OrthographicSize = 3;
+            mainCamera.GetComponent<CinemachineBrain>().enabled = true;
+
+
+        }
+    }
 }
