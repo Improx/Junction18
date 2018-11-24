e2a406e4bcef0f43c9fa80b53a1b4c371a078e75
Max Samarin <maxim.samarin@gmail.com>
2018-11-24 11:46:23 +0200

    almsot working

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -7,15 +7,20 @@ public class FieldOfView : MonoBehaviour {
 	public float Angle = 75;
 	public float MaxDistance = 5;
 	public float Resolution = 100;
+	public MeshFilter ViewMeshFilter;
+
+	private Mesh _viewMesh;
 
 	// Use this for initialization
 	void Start () {
-		
+		_viewMesh = new Mesh();
+		_viewMesh.name = "View Mesh";
+		ViewMeshFilter.mesh = _viewMesh;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+		DrawFieldOfView();
 	}
 
 	private void DrawFieldOfView()
@@ -24,15 +29,48 @@ public class FieldOfView : MonoBehaviour {
 		float viewDirectionAngle = Mathf.Atan2(viewDir.y, viewDir.x);
 		float lowAngle = viewDirectionAngle - Angle / 2;
 		float highAngle = viewDirectionAngle + Angle / 2;
-		int numRays = Mathf.RoundToInt(Angle / Resolution);
+		int numRays = Mathf.RoundToInt(Angle * Resolution);
 		float angleIncrement = Angle / numRays;
+
+		//Shoot rays and save hit points:
+		List<Vector3> hitPoints = new List<Vector3>();
 		for (int i = 0; i < numRays; i++)
 		{
 			float rayAngle = lowAngle + angleIncrement * i;
 			Vector3 rayDir = AngleToDirection(rayAngle);
 			RaycastHit hit;
-			Physics.Raycast(transform.position, viewDir, out hit);
+			bool didHit = Physics.Raycast(transform.position, rayDir, out hit);
+			if (didHit)
+			{
+				hitPoints.Add(transform.InverseTransformPoint(hit.point));
+			}
+			else
+			{
+				hitPoints.Add(transform.InverseTransformPoint((transform.position + rayDir * MaxDistance)));
+			}
 		}
+
+		//Draw flashlight mesh:
+		int vertexCount = hitPoints.Count + 1;
+		Vector3[] vertices = new Vector3[vertexCount];
+		int[] triangles = new int[(vertexCount-2) * 3];
+		vertices[0] = Vector3.zero;
+		for (int i = 0; i < vertexCount - 1; i++)
+		{
+			vertices[i + 1] = hitPoints[i];
+
+			if (i < vertexCount - 2)
+			{
+				triangles[i * 3] = 0;
+				triangles[i * 3 + 1] = i + 1;
+				triangles[i * 3 + 2] = i + 2;
+			}
+		}
+		
+		_viewMesh.Clear();
+		_viewMesh.vertices = vertices;
+		_viewMesh.triangles = triangles;
+		_viewMesh.RecalculateNormals();
 	}
 
 	public Vector3 AngleToDirection(float worldAngle)
