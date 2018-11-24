d5b347a657c460bd86b8061719ce359124cc3b42
Oskiii <oliukku@gmail.com>
2018-11-24 20:55:48 +0200

    Pixel perfect camera for robbers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,40 @@
+using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Player : NetworkBehaviour {
+public class Player : NetworkBehaviour
+{
     public PlayerType Team;
+
+    public bool IsLocalPlayer
+    {
+        get
+        {
+            return isLocalPlayer;
+        }
+    }
+
+    public static PlayerType LocalPlayerTeam
+    {
+        get
+        {
+            var players = new List<Player>(FindObjectsOfType<Player>());
+            var localPlayer = players.Find(x => x.IsLocalPlayer == true);
+            return localPlayer.Team;
+        }
+    }
 
     public CinemachineVirtualCamera RobberCameraPrefab;
 
-	private Robber _robber;
+    private Robber _robber;
 
-    private void Start() {
+    private void Start()
+    {
         if (!isLocalPlayer) return;
 
-        if (Team == PlayerType.Robber) {
+        if (Team == PlayerType.Robber)
+        {
             var mainCamera = FindObjectOfType<Camera>();
 
             var vmCam = Instantiate(RobberCameraPrefab, new Vector3(0, 0, -200), new Quaternion());
@@ -24,28 +46,30 @@ public class Player : NetworkBehaviour {
             vmCam.m_Lens.OrthographicSize = 3;
             mainCamera.GetComponent<CinemachineBrain>().enabled = true;
 
-			_robber = gameObject.AddComponent<Robber>();
+            _robber = gameObject.AddComponent<Robber>();
         }
 
-		else
-		{
-			//We are Guard
+        else
+        {
+            //We are Guard
 
-		}
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         var otherPlayer = other.rigidbody.GetComponent<Player>();
 
         if (!otherPlayer) return;
 
-        if (otherPlayer
-            && otherPlayer.Team == PlayerType.Robber
-            && Team == PlayerType.Guard) {
+        if (otherPlayer &&
+            otherPlayer.Team == PlayerType.Robber &&
+            Team == PlayerType.Guard)
+        {
             GameManager.Instance.CmdCapture(this.gameObject, otherPlayer.gameObject);
         }
     }
-    
+
     [ClientRpc]
     public void RpcGetCaptured()
     {
@@ -53,11 +77,11 @@ public class Player : NetworkBehaviour {
         mover.enabled = false;
     }
 
-	private void Update()
-	{
-		if (isLocalPlayer && Team == PlayerType.Robber)
-		{
-			FlashScreenOverlay.SetAmount(_robber.FlashlightRadiance);
-		}
-	}
+    private void Update()
+    {
+        if (isLocalPlayer && Team == PlayerType.Robber)
+        {
+            FlashScreenOverlay.SetAmount(_robber.FlashlightRadiance);
+        }
+    }
 }
