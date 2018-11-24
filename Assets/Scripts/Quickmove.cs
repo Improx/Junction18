b2eb7763699a6e8c8b79209c683fc07205445528
Lauri Lappalainen <Lawri@users.noreply.github.com>
2018-11-24 14:10:53 +0200

    fastndirty movement
    cuz jukki is too complicated

---
+++ Assets/Scripts/Quickmove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quickmove : MonoBehaviour {
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+        var y = Input.GetAxis("Vertical")* speed *Time.deltaTime;
+        transform.Translate(x, y, 0);
+	}
+}
